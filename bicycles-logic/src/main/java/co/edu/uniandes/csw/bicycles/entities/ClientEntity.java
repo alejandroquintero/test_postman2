f39611afb07fcd3fff3d810c811f420333660a1b
SIS\jd.runza <jd.runza@uniandes.edu.co>
2017-09-17 21:19:41 -0500

    Parte 2

--- bicycles-logic/src/main/java/co/edu/uniandes/csw/bicycles/entities/ClientEntity.java
+++ bicycles-logic/src/main/java/co/edu/uniandes/csw/bicycles/entities/ClientEntity.java
@@ -15,13 +15,65 @@ import javax.persistence.Entity;
  */
 @Entity
 public class ClientEntity extends BaseEntity implements Serializable {
-    private String description;
+    
+    
+    
+    private String lastName;
+    private String firstName;
+    private String login;
+    private String phone;
+    private String address;
+    private String email;
 
-    public String getDescription(){
-        return description;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setDescription(String description){
-        this.description = description;
+    public String getFirstName() {
+        return firstName;
     }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+
+    
 }
